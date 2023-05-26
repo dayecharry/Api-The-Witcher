8d2771ae1240d67a44fdd846fcf0ac5f243469b1
Shurjish <Cperezbarroso@gmail.com>
2023-05-26 16:41:00 +0200

    db config

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connect = async ()=> {
     try {
         const db = await mongoose.connect(process.env.DB_URL);
-        const {name, host} = db.connection;
+        // const {name, host} = db.connection;
     } catch (error) {
         console.log(`Error connecting to DDBB ${error}`);
     }
